03f62ddff08e8386251bb9e935cb1ed9f5594446
Vinod Bhosale <vinod.bhosale@vhaashtech.com>
2022-09-11 21:27:00 +0530

    Add TransportController

---
+++ Clients/MVC/VHaaShUI/Controllers/TransportHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class TransportHomeController : Controller
+    {
+        // GET: TransportHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
