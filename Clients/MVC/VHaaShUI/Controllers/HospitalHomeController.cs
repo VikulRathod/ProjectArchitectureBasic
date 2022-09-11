19b2d73565bfc637771bd741e939693e2a3d287c
vikul rathod <vikul.rathod.net@gmail.com>
2022-09-11 16:25:52 +0530

    Added UI template

---
+++ Clients/MVC/VHaaShUI/Controllers/HospitalHomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class HospitalHomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        public ActionResult Price()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
