1ece2d467bef4fc5824907542d03b180b71f428e
Nitin Kharat <nitin.kharat@vhaashtech.com>
2022-09-13 13:46:54 +0530

    Changes done

---
+++ Clients/MVC/VHaaShUI/Controllers/HotelUiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class HotelUiController : Controller
+    {
+        // GET: HotelUi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult services()
+        {
+            return View();
+        }
+
+        public ActionResult gallery()
+        {
+            return View();
+        }
+        public ActionResult restaurant()
+        {
+            return View();
+        }
+        public ActionResult testimonials()
+        {
+            return View();
+        }
+        public ActionResult booking()
+        {
+            return View();
+        }
+    }
+}
