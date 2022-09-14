f1f04b908c891077482e461d12df40c14187fd74
Avinash Jangle <avinash.jangle@vhaashtech.com>
2022-09-14 09:27:10 +0530

    Logging controller added

---
+++ Clients/MVC/VHaaShUI/Controllers/LoggingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class LoggingController : Controller
+    {
+        // GET: Logging
+        public ActionResult Index()
+        {
+            //Logging controller added
+            return View();
+        }
+    }
+}
