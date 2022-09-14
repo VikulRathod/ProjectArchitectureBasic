e046591060c3dec02c65eef3fa2538d1172f188e
ManojMangore <dotnetmanojkumar@gmail.com>
2022-09-14 10:02:19 +0530

    Test Gym commit for controller and view

---
+++ Clients/MVC/VHaaShUI/Controllers/GymMasterHomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class GymMasterHomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+    }
+}
