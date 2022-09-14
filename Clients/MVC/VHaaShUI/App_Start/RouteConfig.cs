9d21fe0992e06fab73dfed3984fb1dfae35b59b7
dhananjaysatishgaikwad@gmail.com <dhananjaysatishgaikwad@gmail.com>
2022-09-13 22:55:38 -0700

    HospitalManagementSyatem Done

--- Clients/MVC/VHaaShUI/App_Start/RouteConfig.cs
+++ Clients/MVC/VHaaShUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VHaaSh.WEB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "HotelUi", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HospitalHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Clients/MVC/VHaaShUI/Controllers/HospitalHomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class HospitalHomeController : Controller
+    {
+
+        // GET: HospitalHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Doctors()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Blog_Details()
+        {
+            return View();
+            
+        }
+        public ActionResult Blog()
+        {
+            return View();
+            
+        }
+        public ActionResult About()
+        {
+            return View();
+
+        }
+    }
+}
