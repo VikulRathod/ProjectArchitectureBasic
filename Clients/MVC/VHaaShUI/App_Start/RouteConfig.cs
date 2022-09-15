ba2c0c16327e12fa4ddd5f659d7d10450e3dae71
ManojMangore <dotnetmanojkumar@gmail.com>
2022-09-15 18:38:28 +0530

    Test commit

--- Clients/MVC/VHaaShUI/App_Start/RouteConfig.cs
+++ Clients/MVC/VHaaShUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VHaaSh.WEB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "GymMasterHome", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GynastHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
