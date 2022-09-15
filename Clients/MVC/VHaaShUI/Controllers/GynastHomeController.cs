b72dbe2a9c02133fb35302b9c9374b03f6927d1d
ManojMangore <dotnetmanojkumar@gmail.com>
2022-09-15 19:02:20 +0530

    test Commit

---
+++ Clients/MVC/VHaaShUI/Controllers/GynastHomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VHaaSh.WEB.Controllers
+{
+    public class GynastHomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Class()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Feature()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
