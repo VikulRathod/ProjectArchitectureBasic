4e53e982ddb1ceffdcd3ca2770259e6b362011f5
vikul rathod <vikul.rathod.net@gmail.com>
2022-11-25 12:26:42 +0530

    added a datateime helper in utilities

---
+++ Server/WEB API/VHaaSh.Utilities/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VHaaSh.Utilities
+{
+    public class DateTimeHelper : IDateTimeHelper
+    {
+        public int DifferenceWithToday(DateTime inputDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
